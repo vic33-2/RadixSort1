d5737b554df34462950fb8a27ba2bff75abfe6c9
Victor Alan <victoralan.gonzalez8@gmail.com.com>
2025-11-22 18:50:49 -0600

    radix 1

---
+++ RadixSort.java
@@ -0,0 +1,79 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class RadixSort {
+
+
+    public static void main(String[] args) {
+
+        String archivoEntrada = "numeros.txt";
+        String archivoSalida = "numeros_ordenados_radix.txt";
+
+        System.out.println("Leyendo archivo: " + archivoEntrada);
+        int[] numeros = leerArchivo(archivoEntrada);
+
+        System.out.println("\n numeros originales:");
+        imprimirArray(numeros);
+
+        System.out.println("\n Se esta ejecutando RadixSort (se ordena en tiempo real):");
+        radixSort(numeros);
+
+        System.out.println("\n numeros ordenados:");
+        imprimirArray(numeros);
+
+        guardarArchivo(archivoSalida, numeros);
+        System.out.println("\n Archivo ordenado guardado como: " + archivoSalida);
+
+    }
+
+
+    public static int[] leerArchivo(String nombreArchivo) {
+        ArrayList<Integer> numeros = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(nombreArchivo))) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                String[] valores = linea.split("[,\\s]+");
+                for (String v : valores) {
+                    if (!v.trim().isEmpty()) {
+                        numeros.add(Integer.parseInt(v.trim()));
+                    }
+                }
+            }
+        } catch (IOException | NumberFormatException e) {
+            System.out.println("Error al leer el archivo: " + e.getMessage());
+            System.exit(1);
+        }
+        return numeros.stream().mapToInt(i -> i).toArray();
+    }
+
+    public static int obtenerMaximo(int[] arr) {
+        int max = arr[0];
+        for (int num : arr) {
+            if (num > max) max = num;
+        }
+        return max;
+    }
+
+
+    public static void guardarArchivo(String nombreArchivo, int[] datos) {
+        try (FileWriter fw = new FileWriter(nombreArchivo)) {
+            for (int num : datos) {
+                fw.write(num + " ");
+            }
+        } catch (IOException e) {
+            System.out.println("Error al guardar archivo: " + e.getMessage());
+        }
+    }
+
+    public static void imprimirArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
+//.
